3d0887cc1e21ccf2576c519e523bba12fcb74695
ddram2604-sudo <ddram.2604@gmail.com>
2026-07-09 20:18:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Our Mailbox
+const popup = document.getElementById("popup");
+
+document.addEventListener("keydown", function(e){
+
+    if(e.key==="Escape"){
+
+        popup.style.display="none";
+
+    }
+
+});
